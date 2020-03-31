dec0bc4952565cf6263f5c6cd62bdc30232f4dea
nehal-elsayed <62134320+nehal-elsayed@users.noreply.github.com>
2020-03-31 02:15:15 -0700

    add item created

--- resources/views/admin/item/create.blade.php
+++ resources/views/admin/item/create.blade.php
@@ -14,12 +14,30 @@
                 <div class="card-body">
                 
                 
-<form class="input-group" method="POST" action="{{route('category.store')}}">
+                  <form class="input-group" method="POST" action="{{route('item.store')}}">
 @csrf
-                    <input type="text" class="form-control" placeholder="category name" name="title"><br>
-                    <input type="text" class="form-control" placeholder="description"  name="description" >
+                      <label class="control-label">Category</label>
+                      <select class="form-control" name="category">
+                            @foreach($categories as $category)
+                              <option value="{{ $category->id }}">{{ $category->name }}</option>
+                            @endforeach
+                      </select>
+
+                      <label class="control-label">Name</label><br>
+                      <input type="text" class="form-control" name="name"><br>
+
+                      <label class="control-label"> Description </label> <br> 
+                      <textarea class="form-control" name="description"></textarea><br>
+                      <!-- <input type="text" class="form-control" placeholder="description" name="description" ><br> -->
+
+                      <label class="control-label">Price</label><br>
+                      <input type="number" class="form-control" name="price"><br>
+
+                      <label class="control-label">Image</label><br>
+                      <input type="file" name="image"><br>
+                    
                     <span class="input-group-btn">
-                      <a href="{{ route('category.index') }}" class="btn btn-danger">Back</a>
+                      <a href="{{ route('item.index') }}" class="btn btn-danger">Back</a>
                       <button class="btn btn-success btn-xs" type="submit" >Add</button>
                     </span>
 </form>
